523ecbdfe4990ecf8819827c25e793be46f4cab2
Nick Markwell <nick@duckinator.net>
2012-08-09 12:02:03 -0400

    Initial commit.

---
+++ innertext.js
@@ -0,0 +1,34 @@
+if ( (!('innerText' in document.body)) && ('getSelection' in window) ) {
+    HTMLElement.prototype.__defineGetter__("innerText", function() {
+        var selection = window.getSelection(),
+            ranges    = [],
+            str;
+
+        // Save existing selections.
+        for (var i = 0; i < selection.rangeCount; i++) {
+            ranges[i] = selection.getRangeAt(i);
+        }
+
+        // Deselect everything.
+        selection.removeAllRanges();
+
+        // Select `el` and all child nodes
+        selection.selectAllChildren(el);
+
+        // Get the string representation of the selected nodes.
+        str = selection.toString();
+
+        // Deselect everything. Again.
+        selection.removeAllRanges();
+
+        // Restore all formerly existing selections.
+        for (var i = 0; i < ranges.length; i++) {
+            selection.addRange(ranges[i]);
+        }
+
+        // Oh look, this is what we wanted.
+        // String representation of the element, close to as rendered.
+        return str;
+    })
+}
+
